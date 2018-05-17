89f7c096a6cab7f70f45e41f6e5a1ce5907d4234
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2018-05-17 06:11:42 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Person {
+	int id;
+	char * name;
+};
+
+void init(struct Person * p, int id, char * name)
+{
+
+}
+
+void display(struct Person * p)
+{
+	printf("Id   : %d\n", p->id);
+	printf("Name : %s\n", p->name);
+}
+
+int main(int argc, char ** argv)
+{
+	printf("---\n");
+	struct Person tom;
+	tom.id = 0;
+	tom.name = malloc(sizeof(char));
+	strcpy(tom.name, "Tom");
+	display(&tom);
+}
