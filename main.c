43574b401612035519fbda7b2aa71120415d3a79
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2018-05-17 09:01:13 +0200

    Vcard header / footer generation, minimal Person init and free

--- main.c
+++ main.c
@@ -2,29 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Person {
-	int id;
-	char * name;
+#define vCardVersion "3.0"
+#define headerSize 25
+#define footerSize 10
+
+
+struct VCard {
+	char * filename;
+	char * header;
+	struct Person * person;
+	char * footer;
 };
 
-void init(struct Person * p, int id, char * name)
-{
+struct Person {
+	char * lastname;
+	char * firstname;
+};
 
+struct Person * init(char * lastname, char * firstname)
+{
+	struct Person * person = malloc(sizeof(struct Person));
+	person->lastname = malloc(strlen(lastname) * sizeof(char));
+	person->firstname = malloc(strlen(firstname) * sizeof(char));
+	return person;
+}
+
+void freePerson(struct Person * person) {
+	free(person->lastname);
+	free(person->firstname);
+	free(person);
+}
+
+char * generateHeader()
+{
+	char * begin = malloc(headerSize * sizeof(char));
+	strcpy(begin, "BEGIN:VCARD\n");
+	strcat(begin, "VERSION:");
+	strcat(begin, vCardVersion);
+	strcat(begin, "\n");
+	return begin;
+}
+
+char * generateFooter()
+{
+	char * footer = malloc(footerSize * sizeof(char));
+	strcpy(footer, "END:VCARD\n");
+	return footer;
 }
 
 void display(struct Person * p)
 {
-	printf("Id   : %d\n", p->id);
-	printf("Name : %s\n", p->name);
+	printf("Lastname : %s\n", p->lastname);
 }
 
 int main(int argc, char ** argv)
 {
-	printf("---\n");
-	struct Person tom;
-	tom.id = 0;
-	tom.name = malloc(sizeof(char));
-	strcpy(tom.name, "Tom");
-	display(&tom);
-	free(tom.name);
+	char * header = generateHeader();
+	char * footer = generateFooter();
+	printf("%s", header);
+	printf("%s", footer);
+	struct Person * p = init("Morane", "Bob");
+	free(header);
+	free(footer);
+	freePerson(p);
 }
